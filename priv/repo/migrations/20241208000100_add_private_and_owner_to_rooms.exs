31ee7d2ff58177975c212b326db662783e793ea2
nomideusz <b.dymet@gmail.com>
2025-12-08 14:41:43 +0100

    Add rooms migration for is_private and owner_id

---
+++ priv/repo/migrations/20241208000100_add_private_and_owner_to_rooms.exs
@@ -0,0 +1,13 @@
+defmodule Friends.Repo.Migrations.AddPrivateAndOwnerToRooms do
+  use Ecto.Migration
+
+  def change do
+    alter table(:friends_rooms) do
+      add_if_not_exists :is_private, :boolean, default: false, null: false
+      add_if_not_exists :owner_id, references(:friends_users, on_delete: :nilify_all)
+    end
+
+    create_if_not_exists index(:friends_rooms, [:owner_id])
+  end
+end
+
