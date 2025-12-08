482ad04e023f9dd884791652d0724e1ee986d3a7
nomideusz <b.dymet@gmail.com>
2025-12-08 15:42:05 +0100

    Seed invite NOM1 for initial user

---
+++ priv/repo/migrations/20241208000300_seed_nom_invite.exs
@@ -0,0 +1,16 @@
+defmodule Friends.Repo.Migrations.SeedNomInvite do
+  use Ecto.Migration
+
+  def up do
+    execute("""
+    INSERT INTO friends_invites (code, status, inserted_at, updated_at)
+    VALUES ('NOM1', 'active', now(), now())
+    ON CONFLICT DO NOTHING;
+    """)
+  end
+
+  def down do
+    execute("DELETE FROM friends_invites WHERE code = 'NOM1';")
+  end
+end
+
